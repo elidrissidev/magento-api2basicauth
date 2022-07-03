ff727185e50fead617b8880b5ea70dd903c4289a
Mohamed ELIDRISSI <67818913+elidrissidev@users.noreply.github.com>
2022-07-03 11:05:05 +0100

    Show non-visible customer and address attributes

--- app/code/local/Mage/Customer/Model/Api2/Customer/Rest/Restuser/V1.php
+++ app/code/local/Mage/Customer/Model/Api2/Customer/Rest/Restuser/V1.php
@@ -2,4 +2,11 @@
 
 class Mage_Customer_Model_Api2_Customer_Rest_Restuser_V1 extends Mage_Customer_Model_Api2_Customer_Rest_Admin_V1
 {
+    /**
+     * @inheritdoc
+     */
+    protected function _getResourceAttributes()
+    {
+        return $this->getEavAttributes(false, true);
+    }
 }
